03dc4256468bea628e69385a720f051be34a10e2
Elsa <ehazzim@gmail.com>
2023-09-17 15:34:08 +0100

    Setting up game and adjusting screen size and bg

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """Class to store all settings for game"""
+
+    def __init__(self):
+        """Initalize game settings"""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
